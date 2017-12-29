2219fdde98714c6ae53f285ac086773ddbcb5151
Richard Copenhaver <richard.copenhaver@gmail.com>
2017-12-29 18:28:05 -0500

    Beginning after episode 6

---
+++ Jakefile.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	desc("Default build");
+	task("default", function() {
+		console.log("\n\nBUILD OK");
+	});
+
+}());
